713fe1e268f4ac30b1f11c7316cae0838b4f90fe
Christoph Raditsch <christoph@raditsch.info>
2026-04-20 21:43:35 +0200

    fix: prevent shell expansion in AppleScript heredocs
    Content with $(), backticks, or special characters in note body caused
    bash to expand them inside unquoted heredocs (<<EOF) before passing to
    osascript, resulting in syntax errors like (-2741).
    
    Switch all AppleScript functions to quoted heredocs (<<'APPLESCRIPT')
    with on-run-argv to pass values as arguments instead of interpolation.

--- src/lib/update_note.sh
+++ src/lib/update_note.sh
@@ -2,27 +2,28 @@ update_note() {
 	local note_id="$1"
 	local html_content="$2"
 	
-	# Escape double quotes for AppleScript string
-	local escaped_content="${html_content//\"/\\\"}"
-	
-	result=$(osascript 2>&1 <<EOF
-tell application "Notes"
-  try
-    set deletedNotesFolder to folder "Recently Deleted"
-    set theNote to first note whose id is "$note_id"
-    set theFolder to container of theNote
-    
-    if theFolder is equal to deletedNotesFolder then
-      error "Note is in Recently Deleted"
-    end if
-    
-    set body of theNote to "$escaped_content"
-    return "$note_id"
-  on error errMsg
-    error errMsg
-  end try
-end tell
-EOF
+	result=$(osascript 2>&1 - "$note_id" "$html_content" <<'APPLESCRIPT'
+on run argv
+  set noteId to item 1 of argv
+  set noteBody to item 2 of argv
+  tell application "Notes"
+    try
+      set deletedNotesFolder to folder "Recently Deleted"
+      set theNote to first note whose id is noteId
+      set theFolder to container of theNote
+      
+      if theFolder is equal to deletedNotesFolder then
+        error "Note is in Recently Deleted"
+      end if
+      
+      set body of theNote to noteBody
+      return noteId
+    on error errMsg
+      error errMsg
+    end try
+  end tell
+end run
+APPLESCRIPT
 )
 	
 	# Check if result matches the note_id we tried to update
